e6748b13163b6d1ed150a99d2a3760c9c3b82ff1
Sam McGhee <samhmcg5@vt.edu>
2018-04-18 14:35:51 -0400

    database files

---
+++ app/src/main/java/com/example/desve/attendancegps/DatabaseManager.java
@@ -0,0 +1,53 @@
+package com.example.desve.attendancegps;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Samuel McGhee on 4/18/2018.
+ */
+
+public class DatabaseManager {
+
+    private DBOpenHelper dbOpenHelper;
+    private SQLiteDatabase sqLiteDatabase;
+
+    public DatabaseManager(Context context) {
+        dbOpenHelper = new DBOpenHelper(context);
+    }
+
+    public void open() {
+        sqLiteDatabase = dbOpenHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        sqLiteDatabase.close();
+    }
+
+    public void insertUserInfo(int idnum, String username, String password) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DBOpenHelper.COLUMN_USERID, idnum);
+        contentValues.put(DBOpenHelper.COLUMN_USERNAME, username);
+        contentValues.put(DBOpenHelper.COLUMN_PASSWORD, password);
+        sqLiteDatabase.insert(DBOpenHelper.TABLE_NAME, null, contentValues);
+    }
+
+    public UserInfo getUserFromDB() {
+        Cursor cursor = sqLiteDatabase.query(DBOpenHelper.TABLE_NAME,
+                new String[]{DBOpenHelper.COLUMN_ID, DBOpenHelper.COLUMN_USERID,
+                        DBOpenHelper.COLUMN_USERNAME, DBOpenHelper.COLUMN_PASSWORD},
+                null, null, null, null, null);
+        cursor.moveToFirst();
+        UserInfo userInfo = new UserInfo(cursor.getInt(1), cursor.getString(2), cursor.getString(3));
+        return userInfo;
+    }
+
+    public void deleteAll() {
+        if (sqLiteDatabase.isOpen()) {
+            sqLiteDatabase.execSQL("DELETE FROM " + DBOpenHelper.TABLE_NAME);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/desve/attendancegps/UserInfo.java
@@ -0,0 +1,17 @@
+package com.example.desve.attendancegps;
+
+/**
+ * Created by Samuel McGhee on 4/18/2018.
+ */
+
+public class UserInfo {
+    int m_id;
+    String m_username;
+    String m_password;
+
+    public UserInfo(int id, String username, String pass) {
+        m_id = id;
+        m_username = username;
+        m_password = pass;
+    }
+}
