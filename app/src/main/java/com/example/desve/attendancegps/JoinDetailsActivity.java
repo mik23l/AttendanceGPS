ab7909291d45eeff26e9d92892167a96a3793391
Sam McGhee <samhmcg5@vt.edu>
2018-04-20 14:05:32 -0400

    some join details

--- app/src/main/java/com/example/desve/attendancegps/JoinDetailsActivity.java
+++ app/src/main/java/com/example/desve/attendancegps/JoinDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.example.desve.attendancegps;
 
 import android.content.Intent;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,11 +13,12 @@ import com.android.volley.VolleyError;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class JoinDetailsActivity extends AppCompatActivity implements Response.Listener<String>, Response.ErrorListener {
+public class JoinDetailsActivity extends AppCompatActivity implements Response.Listener<String>, Response.ErrorListener, SwipeRefreshLayout.OnRefreshListener {
 
     TextView meetingName;
     MeetingInfo meetingInfo;
     ServerAPI serverAPI;
+    SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +28,18 @@ public class JoinDetailsActivity extends AppCompatActivity implements Response.L
         serverAPI = new ServerAPI(this);
 
         meetingName = findViewById(R.id.meeting_name);
+        swipeRefreshLayout = findViewById(R.id.refreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(this);
 
         Intent intent = getIntent();
         meetingInfo = (MeetingInfo) intent.getSerializableExtra("MEETING");
 
         serverAPI.joinMeeting(String.valueOf(meetingInfo.getId()), "5648554290839552");
         meetingName.setText(meetingInfo.getName());
+    }
+
+    private void handleRefresh() {
+
     }
 
     @Override
@@ -51,4 +59,10 @@ public class JoinDetailsActivity extends AppCompatActivity implements Response.L
         }
     }
 
+    @Override
+    public void onRefresh() {
+        Log.d("DEBUG", "refreshing");
+        handleRefresh();
+        swipeRefreshLayout.setRefreshing(false);
+    }
 }
