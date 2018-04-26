8285f97070094e9d17442f85d917a09e2a5f4bc4
Sam McGhee <samhmcg5@vt.edu>
2018-04-26 11:39:26 -0400

    more user rates xml

--- app/src/main/java/com/example/desve/attendancegps/UserRatesActivity.java
+++ app/src/main/java/com/example/desve/attendancegps/UserRatesActivity.java
@@ -2,6 +2,7 @@ package com.example.desve.attendancegps;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class UserRatesActivity extends Activity {
 
@@ -9,5 +10,8 @@ public class UserRatesActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_rates);
+    }
+
+    public void onClickFilter(View view) {
     }
 }
